53d30589e08ac90de52ce83a44d88123eda4ee61
Alexey <alexey6486@gmail.com>
2020-10-02 14:03:56 +0300

    dinamic-breadcrumbs

--- src/components/breadcrumbs/breadcrumbs.component.tsx
+++ src/components/breadcrumbs/breadcrumbs.component.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import {Breadcrumbs, CurrentCrumb, LinkCrumb} from "./breadcrumbs.styles";
 
-export const BreadcrumbsComponent = () => {
+type PropsType = {
+    pagesArr: String[];
+}
+
+export const BreadcrumbsComponent = (props: PropsType) => {
+
+    const {pagesArr} = props;
+
+    const pagesMap = pagesArr.map((page, idx) => {
+        if (pagesArr.length > 1 && idx != pagesArr.length - 1) {
+            return <LinkCrumb>{page}/</LinkCrumb>
+        } else if (pagesArr.length > 1 && idx === pagesArr.length - 1) {
+            return <CurrentCrumb key={page}>{page}</CurrentCrumb>
+        } else {
+            return <CurrentCrumb key={page}>{page}</CurrentCrumb>
+        }
+    })
 
     return (
         <Breadcrumbs>
-            <LinkCrumb>Главная</LinkCrumb>/
-            <CurrentCrumb>Личный кабинет</CurrentCrumb>
+            {pagesMap}
         </Breadcrumbs>
     )
 }
--- src/components/pageInfo/pageInfo.component.tsx
+++ src/components/pageInfo/pageInfo.component.tsx
@@ -3,12 +3,15 @@ import { BreadcrumbsComponent } from "../breadcrumbs/breadcrumbs.component";
 import {PageInfo, PageTitle} from "./pageInfo.styles";
 
 export const PageInfoComponent = () => {
+
+    const pagesArr = ['Главная', 'Личный кабинет']
+
     return (
         <PageInfo>
             <PageTitle>
                 ЛИЧНЫЙ ПРОФИЛЬ
             </PageTitle>
-            <BreadcrumbsComponent/>
+            <BreadcrumbsComponent pagesArr={pagesArr}/>
         </PageInfo>
     )
 }
